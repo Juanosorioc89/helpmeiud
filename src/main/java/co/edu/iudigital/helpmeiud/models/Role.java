ea48774a510ab91b09e0d77214e1bcca46f9ea65
Juan David Osorio <juan.osorioc@est.iudigital.edu.co>
2024-07-16 12:19:49 -0500

    :construction: :sparkles: properties & models & AX

---
+++ src/main/java/co/edu/iudigital/helpmeiud/models/Role.java
@@ -0,0 +1,27 @@
+package co.edu.iudigital.helpmeiud.models;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Role implements Serializable {
+
+    static final long serialVersionIUD = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    @Column(unique = true, length = 100)
+    String nombre;
+    @Column
+    String descripcion;
+
+}
